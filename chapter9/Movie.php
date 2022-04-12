0174e4a73ecad5e09bfc569ee745f3d09d15d039
SonTrungTo <son.trung.to@gmail.com>
2022-04-12 11:52:49 +0300

    chap9: getter and setter

---
+++ chapter9/Movie.php
@@ -0,0 +1,53 @@
+<?php
+
+class Movie {
+    private $id;
+    public $title;
+    private $rentalPrice;
+    const DISCOUNT = 10;
+
+    public function __construct($pId, $pTitle, $pRentalPrice)
+    {
+        $this->id = $pId;
+        $this->title = $pTitle;
+        $this->rentalPrice = $pRentalPrice;
+    }
+
+    public function conversion($country)
+    {
+        $rate = 1;
+        switch($country) {
+            case 'UK':
+                $rate = 0.76;
+                break;
+            case 'Japan':
+                $rate = 110;
+                break;
+        }
+        return round($rate*$this->rentalPrice, 2);
+    }
+
+    public function displayHeading($tag)
+    {
+        if (substr($this->id, 0, 1) == 'N') {
+            return "<$tag>Movies</$tag>";
+        } else {
+            return "<$tag>Award Winning Movies</$tag>";
+        }
+    }
+
+    public function __get($propertyRequested)
+    {
+        if ($propertyRequested == 'id') {
+            # code...
+        } else {
+            # code...
+        }
+        
+    }
+
+    public function __set($propertyToModify, $value)
+    {
+        # code...
+    }
+}
---
+++ chapter9/README.md
@@ -0,0 +1,8 @@
+## OOP
+- Grouping related variables, constants and functions into a class =>
+objects => store data and access functions
+
+## Access modifier
+- `public`: easy access.
+- `private`: only to that class.
+- `protected`: that class and its subclasses.
