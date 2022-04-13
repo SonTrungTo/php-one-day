93ea27a797355d42054b33c38417ce0b40fb566c
SonTrungTo <son.trung.to@gmail.com>
2022-04-13 17:49:31 +0300

    ch11: testing db

--- chap11/db.php
+++ chap11/db.php
@@ -1,4 +1,36 @@
 <?php
-$dsn = 'mysql:';
+$dsn  = 'mysql:host=127.0.0.1;dbname=pawszone';
+$username = 'pz_admin';
+$password = 'ABCD';
 
-$pdo = new PDO();
+try {
+    $pdo = new PDO($dsn, $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+} catch (PDOException $e) {
+    echo 'Connection failed: '.$e->getMessage();
+}
+
+# Create SQL using placeholders
+$sqlUnnamed = "SELECT * FROM pets WHERE owner = ? AND petname = ?";
+$sqlNamed = "SELECT * FROM pets WHERE owner = :owner AND petname = :pname";
+# Prepare the statements
+$prepareUnnamed = $pdo->prepare($sqlUnnamed);
+$prepareNamed = $pdo->prepare($sqlNamed);
+# Binding values to placeholders
+## bindValue() or bindParam()
+$prepareUnnamed->bindValue(2, $myPet);
+$prepareNamed->bindValue(':pname', $myPet);
+# Execute the statement
+$prepareUnnamed->execute();
+$prepareNamed->execute();
+# Fetch the data returned, if any
+## fetch() vs fetchAll()
+## FETCH_NUM vs FETCH_ASSOC
+while ($row = $prepareUnnamed->fetch(PDO::FETCH_NUM)) {
+    echo 'Pet = '.$row[1].'<BR>';
+    echo 'Owner = '.$row[0].'<BR><BR>';
+}
+
+while ($row = $prepareUnnamed->fetch(PDO::FETCH_ASSOC)) {
+    echo 'Pet = '.$row['petname'].'<BR>';
+    echo 'Owner = '.$row['owner'].'<BR><BR>';
+}
